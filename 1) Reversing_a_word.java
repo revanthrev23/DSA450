f6b2691b90f251e8b76714115b152160d993cfc0
revanthrev23 <74725501+revanthrev23@users.noreply.github.com>
2021-01-02 17:48:47 +0530

    Create 1) Reversing_a_word.java

---
+++ 1) Reversing_a_word.java
@@ -0,0 +1,7 @@
+public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        StringBuilder s=new StringBuilder(str);
+        s=s.reverse();
+        return s.toString();
+    }
