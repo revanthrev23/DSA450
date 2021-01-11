63723fb7a8cb4d20a7bdafe29f5a6f46d5749b3a
revanthrev23 <74725501+revanthrev23@users.noreply.github.com>
2021-01-11 20:32:36 +0530

    Create 9) minimizing_max_difference_between_heights_of_tower.java

---
+++ 9) minimizing_max_difference_between_heights_of_tower.java
@@ -0,0 +1,33 @@
+int getMinDiff(int[] arr, int n, int k) {
+        if (n == 1) 
+        return 0; 
+
+        Arrays.sort(arr); 
+  
+        int ans = arr[n-1] - arr[0]; 
+        int small = arr[0] + k; 
+        int big = arr[n-1] - k; 
+        int temp = 0; 
+          
+        if (small > big) 
+        { 
+            temp = small; 
+            small = big; 
+            big = temp; 
+        } 
+  
+        for (int i = 1; i < n-1; i ++) 
+        { 
+            int subtract = arr[i] - k; 
+            int add = arr[i] + k; 
+            if (subtract >= small || add <= big) 
+                continue; 
+            if (big - subtract < add - small) 
+                small = subtract; 
+            else
+                big = add; 
+        } 
+  
+        return Math.min(ans, big - small); 
+        
+    }
